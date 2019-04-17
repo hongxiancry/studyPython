395e42a51ad73896362b839ce532ce4de0653f2d
柴瑞亚 <chairuiya@chairuiyadeMacBook-Air.local>
2019-04-17 18:02:20 -0500

    修改统计字符个数脚本

--- pythonScripts/answer004.py
+++ pythonScripts/answer004.py
@@ -3,20 +3,24 @@
 __author__='chairuiya'
 
 import re
-
+import collections
+from collections import Counter
 
 def statisticWords(file_path):
 	f  = open(file_path,'r')
 	words = f.read()
 	if words:
-		mathces = re.findall(r'[\w\_\-\;\,\.]+',words)
-		return mathces
+		mathces = re.findall(r'[\w\_\-]+',words)
+		for countWords in mathces:
+			counts = collections.Counter(mathces)
+			count = (counts,countWords)[0]
+			i = 0
+			for key in count:
+				i = i+1
+				print('%s:'%(i,),'%s:'%(key,),count[key])
+			return counts,countWords
 	else:
-		return 0
+		return (0,0)
 if __name__=='__main__':
 	test_file_path = 'test0004.txt'
-	testwords = statisticWords(test_file_path)
-	i = 0;
-	for testword in testwords:
-		i = i+1
-		print('%s:'%(i,),testword)
+	testcounts = statisticWords(test_file_path)
