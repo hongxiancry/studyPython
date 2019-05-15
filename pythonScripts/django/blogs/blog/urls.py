086a523af923890a57c95d8545c1aa3231390f2b
柴瑞亚 <chairuiya@chairuiyadeMacBook-Air.local>
2019-05-14 22:08:50 -0500

    提交关于分类功能

--- pythonScripts/django/blogs/blog/urls.py
+++ pythonScripts/django/blogs/blog/urls.py
@@ -6,4 +6,6 @@ app_name='blog'
 urlpatterns = [
 path(r'',views.home,name='home'),
 path(r'^post/(?P<pk>\d+)/$',views.detail,name='detail'),
+path(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$',views.archives,name='archives'),
+path(r'^category/(?P<pk>\d+)/$',views.category,name='category'),
 ]
--- pythonScripts/django/blogs/blog/views.py
+++ pythonScripts/django/blogs/blog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render,get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
-from .models import Post
-
+from .models import Post,Category
+import markdown
 
 def home(request):
 	post_list = Post.objects.all().order_by('-created_Time')
@@ -15,4 +15,19 @@ def home(request):
 
 def detail(request,pk):
 	post = get_object_or_404(Post,pk=pk)
+	post.body = markdown.markdown(post.body,extensions=[
+		'markdown.extensions.extra',
+		'markdown.extensions.codehilite',
+		'markdown.extensions.toc',
+		])
 	return render(request,'blog/detail.html',context={'post':post})
+
+def archives(request,year,month):
+	post_list = Post.objects.filter(created_Time__year=year,
+		created_Time__month=month).order_by('-created_Time')
+	return render(request,'blog/homepage.html',context={'post_list':post_list})
+
+def category(request,pk):
+	cate = get_object_or_404(Category,pk=pk)
+	post_list = Post.objects.filter(category=cate).order_by('-created_Time')
+	return render(request,'blog/homepage.html',context={'post_list':post_list})
